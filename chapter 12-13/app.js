e130443d1aaa014545d9bebff88d06e6cb7551a5
saqibYousuf <saqibyousuf001@gmail.com>
2020-06-10 19:25:21 +0500

    Chapter 12-13

--- chapter 12-13/app.js
+++ chapter 12-13/app.js
@@ -1,62 +1,62 @@
-// // task 2
-// var integer1 = prompt("Enter your First number:")
-// var integer2 = prompt("Enter your second number:")
-// if(integer1 > integer2) {
-//     alert(integer1)
-// } else if (integer2 > integer1){
-//     alert(integer2)
-// }else if (integer1 === integer2){
-//     alert("first number is equal to second number")
-// }
+// task 2
+var integer1 = prompt("Enter your First number:")
+var integer2 = prompt("Enter your second number:")
+if(integer1 > integer2) {
+    alert(integer1)
+} else if (integer2 > integer1){
+    alert(integer2)
+}else if (integer1 === integer2){
+    alert("first number is equal to second number")
+}
  
-// // task 3
-// var number = prompt("Enter Your Number")
-// if (number > 0 ){
-//     alert("Number is Positive")
-// } else if (number == 0 ) {
-//     alert("Your number is zero")
-// } else if(number < 0) {
-//     alert("number is Negative")
-// }
+// task 3
+var number = prompt("Enter Your Number")
+if (number > 0 ){
+    alert("Number is Positive")
+} else if (number == 0 ) {
+    alert("Your number is zero")
+} else if(number < 0) {
+    alert("number is Negative")
+}
 
-// // task 4
-// var vowels = '^[aieouAIEOU].*'
-// var input = prompt("Enter any Word") 
-// var check = input.match(vowels)
-// if (check){
-//     alert("this word is vowel")
-// } else {
-//     alert("this is not vowel")
-// }
+// task 4
+var vowels = '^[aieouAIEOU].*'
+var input = prompt("Enter any Word") 
+var check = input.match(vowels)
+if (check){
+    alert("this word is vowel")
+} else {
+    alert("this is not vowel")
+}
 
-// // task 5
-// var correct_password = "123abc"
-// var enter_password = prompt("Please Enter your Password")
-// if (enter_password === correct_password){
-//     alert("Correct! The password you entered matches the original password")
-// } else if (enter_password !== correct_password){
-//     alert("Password is Incorrect")
-// }else {
-//     alert("please enter your password")
-// }
+// task 5
+var correct_password = "123abc"
+var enter_password = prompt("Please Enter your Password")
+if (enter_password === correct_password){
+    alert("Correct! The password you entered matches the original password")
+} else if (enter_password !== correct_password){
+    alert("Password is Incorrect")
+}else {
+    alert("please enter your password")
+}
 
-// // task 6
-// var greeting; 
-// var hour = 13;
-//  if (hour < 18) { 
-//     greeting = "Good day"; 
-//  }else{ 
-//  greeting = "Good evening"; 
-// } 
+// task 6
+var greeting; 
+var hour = 13;
+ if (hour < 18) { 
+    greeting = "Good day"; 
+ }else{ 
+ greeting = "Good evening"; 
+} 
 
-// // task 7
-// var time = prompt("Enter Time in 24hour clock format")
-// if (time >= "00:00" && time < "12:00"  ){
-//     alert("Good Morning")
-// } else if (time >= "12:00" && time < "17:00"  ){
-//     alert("Good Afternoon")
-// } else if (time >= "17:00" && time < "21:00"  ){
-//     alert("Good Evening")
-// } else if (time >= "21:00" && time < "23:59"  ){
-//     alert("Good Night")
-// } 
+// task 7
+var time = prompt("Enter Time in 24hour clock format")
+if (time >= "00:00" && time < "12:00"  ){
+    alert("Good Morning")
+} else if (time >= "12:00" && time < "17:00"  ){
+    alert("Good Afternoon")
+} else if (time >= "17:00" && time < "21:00"  ){
+    alert("Good Evening")
+} else if (time >= "21:00" && time < "23:59"  ){
+    alert("Good Night")
+} 
---
+++ chapter 14-16/app.js
@@ -0,0 +1,11 @@
+// // task 1
+// var StudentsNames = [];
+
+// //task 2
+// var StudentsNames
+
+// // task 3
+// var str_arr = ['hey', 'hi']
+
+// // task 4
+// var number_arr = [29, 19]
